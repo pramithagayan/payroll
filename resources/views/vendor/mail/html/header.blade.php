b434880f7d048ffe107d9afb397bd73fbe8b7bfe
Lahiru Nanayakkara <projectmanager@bigcitymarketing.net>
2020-05-13 20:23:32 +0530

    Alter Email Logo Size

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            <img src="{{ asset('img/aselogo.png') }}" alt="{{ config('app.name') }}" width="165" height="85"/>
+            <img src="{{ asset('img/aselogo.png') }}" alt="{{ config('app.name') }}" width="165" height="65"/>
         </a>
     </td>
 </tr>
